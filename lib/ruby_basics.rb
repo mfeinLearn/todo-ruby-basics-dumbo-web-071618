a580e350777d0096234f41ccf7b0c16b0cf7d25d
Malcome Fein <mfein90@gmail.com>
2018-07-19 18:02:34 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,8 +18,13 @@ def return_a_value(return_a_value = "Nice")
   "#{return_a_value}"
 end
 
+<<<<<<< HEAD
 def last_evaluated_value(last_evaluated_value = "expert")
     "#{last_evaluated_value}"
+=======
+def last_evaluated_value(return_a_value = "Nice")
+
+>>>>>>> 1e6111420a6af04dddd4b89cfff60a534e2b33be
 end
 
 def pizza_party(pizza_party = "cheese")
